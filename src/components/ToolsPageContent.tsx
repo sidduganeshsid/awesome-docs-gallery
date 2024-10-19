5ca2ae49eeed40d96b6ae158213de88afd895128
fazlerocks <fazle@hashnode.com>
2024-10-19 13:17:23 +0530

    Update ToolsPageContent.tsx

--- src/components/ToolsPageContent.tsx
+++ src/components/ToolsPageContent.tsx
@@ -22,7 +22,7 @@ const docsTools = [
   {
     id: 2,
     title: "Docusaurus",
-    description: "Easily build optimized websites using React for your open-source project.",
+    description: "Docusaurus is an open-source framework that helps developers build, deploy, and maintain documentation websites easily. It supports Markdown, and provides built-in versioning.",
     link: "https://docusaurus.io/",
     logo: "https://cdn.hashnode.com/res/hashnode/image/upload/v1729281269828/PUMyx1KdK.png?auto=format",
     screenshot: "https://cdn.hashnode.com/res/hashnode/image/upload/v1729281215685/lYUHWAWaF.png?auto=format",
@@ -94,6 +94,92 @@ const ToolsPageContent: React.FC = () => {
         <h1 className="text-3xl lg:text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-neutral-950 dark:from-neutral-100 dark:via-neutral-300 via-neutral-600 dark:to-neutral-400 to-neutral-500 text-balance text-center !leading-tight !tracking-tight">
           Best documentation tools for developers.
         </h1>
+        <div className="absolute -z-10 -top-80 right-0 lg:-right-80">
+          <svg
+            fill="none"
+            className="w-[800px] h-auto block dark:hidden"
+            viewBox="0 0 981 862"
+          >
+            <g filter="url(#a)">
+              <path
+                fill="#FAE8FF"
+                d="M359.5 393.5V291l56-127.5 86 53.5h101l72 59.5H775l-52 167H519.5l-160-50Z"
+              />
+              <path
+                fill="#A7F3D0"
+                d="m120.5 436.5 61-109.5h40l28.5 61 91.5 38 23.5 66.5-59 64.5-124.5-45-61-75.5Z"
+              />
+              <path
+                fill="#E0F2FE"
+                d="m252 476 30.5-356 45 99L473 262l90 77.5L766 397l122 37.5L949.5 593 766 729.5 498 742l-246-75.5V476Z"
+              />
+            </g>
+            <defs>
+              <filter
+                id="a"
+                width="1069"
+                height="862"
+                x=".5"
+                y="0"
+                color-interpolation-filters="sRGB"
+                filterUnits="userSpaceOnUse"
+              >
+                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                <feBlend
+                  in="SourceGraphic"
+                  in2="BackgroundImageFix"
+                  result="shape"
+                />
+                <feGaussianBlur
+                  result="effect1_foregroundBlur_1_138"
+                  stdDeviation="60"
+                />
+              </filter>
+            </defs>
+          </svg>
+          <svg
+            fill="none"
+            className="w-[800px] h-auto hidden dark:block"
+            viewBox="0 0 1070 862"
+          >
+            <g filter="url(#a)">
+              <path
+                fill="#86198F"
+                d="M359.5 393.5V291l56-127.5 86 53.5h101l72 59.5H775l-52 167H519.5l-160-50Z"
+              />
+              <path
+                fill="#065F46"
+                d="m120.5 436.5 61-109.5h40l28.5 61 91.5 38 23.5 66.5-59 64.5-124.5-45-61-75.5Z"
+              />
+              <path
+                fill="#075985"
+                d="m252 476 30.5-356 45 99L473 262l90 77.5L766 397l122 37.5L949.5 593 766 729.5 498 742l-246-75.5V476Z"
+              />
+            </g>
+            <defs>
+              <filter
+                id="a"
+                width="1069"
+                height="862"
+                x=".5"
+                y="0"
+                color-interpolation-filters="sRGB"
+                filterUnits="userSpaceOnUse"
+              >
+                <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                <feBlend
+                  in="SourceGraphic"
+                  in2="BackgroundImageFix"
+                  result="shape"
+                />
+                <feGaussianBlur
+                  result="effect1_foregroundBlur_8_8"
+                  stdDeviation="60"
+                />
+              </filter>
+            </defs>
+          </svg>
+        </div>
       </motion.section>
 
       {/* Cards section to list documentation tools */}
